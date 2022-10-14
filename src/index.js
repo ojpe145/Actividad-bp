94984a6b96891182edb545369f4fe8e3280eb7a2
ojpe145 <osnaider_pacheco14@hotmail.com>
2022-10-13 21:56:39 -0500

    Contadr Prueba

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Variables from './components/Variables';
 import Formulario from './components/Formulario';
+import Contador from './components/Contador';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
    
-    <Variables />
+    <Contador />
    
   </React.StrictMode>
 );
